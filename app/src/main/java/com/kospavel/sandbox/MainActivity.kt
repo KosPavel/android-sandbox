2a7bdbca28737fb5811b8498e5918ae1a6889fec
KosPavel <kos.pav007@yandex.ru>
2022-12-06 16:11:20 +0300

    two ways of textView folding example

--- app/src/main/java/com/kospavel/sandbox/MainActivity.kt
+++ app/src/main/java/com/kospavel/sandbox/MainActivity.kt
@@ -1,18 +1,41 @@
 package com.kospavel.sandbox
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import com.kospavel.sandbox.ui.main.MainScreen
-import com.kospavel.sandbox.ui.theme.SandboxTheme
+import com.kospavel.sandbox.utils.TextUtils
+import com.kospavel.sandbox.utils.TextUtilsJava
 
 class MainActivity : ComponentActivity() {
+
+    private var expanded = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SandboxTheme {
-                MainScreen()
+        setContentView(R.layout.layout_main_activity)
+
+        initTextView1()
+        initTextView2()
+    }
+
+    private fun initTextView1() {
+        val textView1 = findViewById<TextView>(R.id.tvTextContent1)
+        textView1.text = TextUtils.text600Symbols
+        textView1.setOnClickListener {
+            textView1.maxLines = if (expanded) {
+                5
+            } else {
+                Int.MAX_VALUE
             }
+            expanded = !expanded
         }
+    }
+
+    private fun initTextView2() {
+        val textView2 = findViewById<TextView>(R.id.tvTextContent2)
+        textView2.text = TextUtils.text1000Symbols
+
+        TextUtilsJava.makeTextViewResizable(textView2, 5, "expand text here", true);
     }
 }
